fbbab8522a3b26fca923ca3aacafd7a86f956390
anilthakur0909 <anil.thakur0909@gmail.com>
2014-01-07 11:16:02 +0530

    added news view pages,css and js pages and migrate database,

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 MyApp::Application.routes.draw do
+  #get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root "welcome#index"
+  
+  resources :questionbanks do
+    collection do
+      get :addQuestions      
+    end
+  end
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
